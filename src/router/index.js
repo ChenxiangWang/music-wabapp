7e1a10ff5349bbe9568755f010ff2d09ed93cfe4
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-23 13:18:26 +0800

    init router

---
+++ src/router/index.js
@@ -0,0 +1,20 @@
+import Mine from '@/pages/mine';
+import Discover from '@/pages/discover';
+import Friend from '@/pages/friend';
+
+const routes = [
+        {
+            path: "mine",
+            element: <Mine/>
+        },
+        {
+            path: "discover",
+            element: <Discover/>
+        },
+        {
+            path: "friend",
+            element: <Friend/>
+        }
+    ]
+;
+export default routes;
