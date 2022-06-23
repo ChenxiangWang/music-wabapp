69c99e301639e1de0f734db4092ad7ca2731997f
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-23 13:19:23 +0800

    init App with router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,16 @@
-import React, { memo } from 'react';
+import React, {memo} from 'react';
+import { useRoutes } from "react-router-dom";
+import routes from './router';
+import MyHeader from '@/components/app-header';
+import MyFooter from '@/components/app-footer';
 
 export default memo(function App() {
+    let element = useRoutes(routes);
     return (
-        <div>
-            App
-        </div>
-    )
+        <>
+            <MyHeader />
+            {element}
+            <MyFooter />
+        </>
+    );
 })
