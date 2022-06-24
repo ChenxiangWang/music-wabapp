d0604fda3cb995458837f99adeae764f865ac67f
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-24 10:33:23 +0800

    Update <Discovered>'s layout

--- src/pages/discover/index.js
+++ src/pages/discover/index.js
@@ -1,9 +1,23 @@
 import React, {memo} from 'react';
+import { NavLink, Outlet } from 'react-router-dom';
+import { DiscoverWrapper, TopMenu } from './style';
+import { dicoverMenu, } from '@/common/local-data';
 
-export default memo(function Discover() {
+export default memo(function Discover(props) {
     return (
-        <div>
-            Discover
-        </div>
+        <DiscoverWrapper>
+            <div className={"top"}>
+                <TopMenu className={"wrap-v1"}>
+                    {
+                        dicoverMenu.map((item, index) => {
+                            return (<div className={"item"} key={item.title}>
+                                <NavLink to={item.link}>{item.title}</NavLink>
+                            </div>)
+                        })
+                    }
+                </TopMenu>
+                <Outlet />
+            </div>
+        </DiscoverWrapper>
     )
 })
