cc729f9510316e293315f29c6059a4bf6c180007
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-26 05:22:57 +0800

    Update reducer with immutable js

--- src/pages/discover/child-pages/recommend/store/reducer.js
+++ src/pages/discover/child-pages/recommend/store/reducer.js
@@ -1,14 +1,22 @@
+import { Map } from 'immutable';
 import * as actionTypes from './constants'
 
-const defaultState = {
-    topBanners: []
-}
+const defaultState = Map({
+    topBanners: [],
+    hotRecommends: [],
+})
 
 export default function reducer(state = defaultState, action) {
+
     switch (action.type) {
+
         case actionTypes.CHANGE_TOP_BANNER:
-            return {...state, topBanners: action.data}
+            return state.set('topBanners', action.data);
+
+        case actionTypes.CHANGE_HOT_RECOMMEND:
+            return state.set('hotRecommends', action.data);
+
         default:
-            return state
+            return state;
     }
 }
