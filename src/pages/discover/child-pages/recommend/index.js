1c2e2dabf430a521013557972ee34983827b8457
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-25 20:46:35 +0800

    Update with useReducer

--- src/pages/discover/child-pages/recommend/index.js
+++ src/pages/discover/child-pages/recommend/index.js
@@ -1,14 +1,20 @@
 import React, { memo,useEffect } from 'react';
-import { useDispatch, useSelector} from 'react-redux';
+import { useDispatch, useSelector, shallowEqual } from 'react-redux';
 import { getTopBannerAction } from './store/action'
 
 function Recommand(props) {
 
     const dispatch = useDispatch();
 
+    /**
+     *  useSelector: 有时候会影响性能
+     *  因为 useSelector 传入的是个函数，这个函数每次返回都是一个新的对象，即使数据本身没有发生改变，也会造成页面的重新更新
+     *  所以，我们一般会传入第二个函数，来定义自己的状态比较
+     * */
+
     const { topBanners } = useSelector((state) => ({
         topBanners: state.recommend.topBanners,
-    }));
+    }),shallowEqual);
 
     useEffect(() => {
         dispatch(getTopBannerAction());
