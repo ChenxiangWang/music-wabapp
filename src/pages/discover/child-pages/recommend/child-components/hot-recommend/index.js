558fc507b936e6c8648bc1b9a1b6e93fc92a46ca
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-26 05:18:13 +0800

    Update page by integrating <SongsCover>

--- src/pages/discover/child-pages/recommend/child-components/hot-recommend/index.js
+++ src/pages/discover/child-pages/recommend/child-components/hot-recommend/index.js
@@ -1,39 +1,37 @@
-import React, { memo, useEffect } from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import React, {memo, useEffect} from 'react';
+import {useDispatch, useSelector, shallowEqual} from 'react-redux';
 
-import { HOT_RECOMMEND_LIMIT } from '@/common/contants';
+import ThemeHeaderRCM from '@/components/theme-header-rcm';
+import SongsCover from '@/components/songs-cover';
 
-import HYThemeHeaderRCM from '@/components/theme-header-rcm';
-import HYSongsCover from '@/components/songs-cover';
 import {
-  HotRecommendWrapper
+    HotRecommendWrapper
 } from './style';
-import { getHotRecommendAction } from '../../store/actionCreators';
+import {getHotRecommendAction} from '../../store/action';
 
-export default memo(function HYHotRecommend() {
-  // state
+export default memo(function HotRecommend() {
+    // state
 
-  // redux hooks
-  const { hotRecommends } = useSelector(state => ({
-    hotRecommends: state.getIn(["recommend", "hotRecommends"])
-  }), shallowEqual);
-  const dispatch = useDispatch();
-
-  // other hooks
-  useEffect(() => {
-    dispatch(getHotRecommendAction(HOT_RECOMMEND_LIMIT));
-  }, [dispatch]);
-
-  return (
-    <HotRecommendWrapper>
-      <HYThemeHeaderRCM title="热门推荐" keywords={["华语", "流行", "民谣", "摇滚", "电子"]} />
-      <div className="recommend-list">
-        {
-          hotRecommends.map((item, index) => {
-            return <HYSongsCover key={item.id} info={item}/>
-          })
-        }
-      </div>
-    </HotRecommendWrapper>
-  )
+    // redux hooks
+    const dispatch = useDispatch();
+    const { hotRecommends } = useSelector( state => ({
+        hotRecommends: state.getIn(['recommend', 'hotRecommends'])
+    }));
+    // other hooks
+   useEffect(() => {
+       dispatch(getHotRecommendAction(8));
+   }, [dispatch])
+    return (
+        <HotRecommendWrapper>
+            <ThemeHeaderRCM title={"热门推荐"} keywords={['华语', '流行', '民谣', '摇滚', '电子']}/>
+            <div className='recommend-list'>
+                {
+                    hotRecommends.map((item, index) => {
+                        console.log(item);
+                        return <SongsCover key={item.id} info={item}/>
+                    })
+                }
+            </div>
+        </HotRecommendWrapper>
+    )
 })
