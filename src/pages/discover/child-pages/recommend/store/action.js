1eb10de0122f790bdfa53627ad41b911a81e87dd
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-26 05:15:42 +0800

    Add recommend related actions

--- src/pages/discover/child-pages/recommend/store/action.js
+++ src/pages/discover/child-pages/recommend/store/action.js
@@ -1,16 +1,31 @@
 import * as actionTypes from './constants';
 
-import { getTopBanners } from '@/services/recommend';
+import { getTopBanners, getHotRecommend } from '@/services/recommend';
 
 const changeTopBannerAction = (res) => ({
     type: actionTypes.CHANGE_TOP_BANNER,
     data: res.banners,
 })
 
+const changeHotRecommendAction = (res) => ({
+    type: actionTypes.CHANGE_HOT_RECOMMEND,
+    data: res.result,
+})
+
 export const getTopBannerAction = function () {
     return dispatch => {
         getTopBanners().then(res => {
             dispatch(changeTopBannerAction(res))
+        })
+    }
+}
+
+
+
+export const getHotRecommendAction = function(limit) {
+    return dispatch => {
+        getHotRecommend(limit).then(res =>  {
+            dispatch(changeHotRecommendAction(res));
         })
     }
 }
