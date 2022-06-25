81c22a61573c0173799b5fb37a2dbe81c1a59012
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-26 05:20:13 +0800

    Update page with <HotRecommend>

--- src/pages/discover/child-pages/recommend/index.js
+++ src/pages/discover/child-pages/recommend/index.js
@@ -1,29 +1,33 @@
-import React, { memo,useEffect } from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
-import { getTopBannerAction } from './store/action'
+import React, { memo } from 'react';
+import TopBanner from './child-components/top-banner';
+import HotRecommend from  './child-components/hot-recommend';
+import NewAlbums from './child-components/new-album';
+import RecomendRanking from './child-components/recommend-ranking';
+
+import {
+    RecommendWrapper,
+    RecommendLeft,
+    RecommendRight,
+    Content,
+} from "./style"
+
+
 
 function Recommand(props) {
 
-    const dispatch = useDispatch();
-
-    /**
-     *  useSelector: 有时候会影响性能
-     *  因为 useSelector 传入的是个函数，这个函数每次返回都是一个新的对象，即使数据本身没有发生改变，也会造成页面的重新更新
-     *  所以，我们一般会传入第二个函数，来定义自己的状态比较
-     * */
-
-    const { topBanners } = useSelector((state) => ({
-        topBanners: state.recommend.topBanners,
-    }),shallowEqual);
-
-    useEffect(() => {
-        dispatch(getTopBannerAction());
-    }, [dispatch]) // componentDidMount.
-
     return (
-        <div>
-            Recommand: { topBanners.length }
-        </div>
+        <RecommendWrapper>
+            <TopBanner />
+            <Content className={"wrap-v2"}>
+                <RecommendLeft>
+                    <HotRecommend />
+                    <NewAlbums />
+                    <RecomendRanking />
+                </RecommendLeft>
+                <RecommendRight>
+                </RecommendRight>
+            </Content>
+        </RecommendWrapper>
     )
 }
 export default memo(Recommand);
