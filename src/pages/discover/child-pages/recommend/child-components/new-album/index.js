cf466b89facc22e7b9a74696a01047bfe7d466c8
chenxiang wang <z5083841@student.unsw.edu.au>
2022-06-30 07:26:59 +0800

    Add <newAlbum />

--- src/pages/discover/child-pages/recommend/child-components/new-album/index.js
+++ src/pages/discover/child-pages/recommend/child-components/new-album/index.js
@@ -1,56 +1,61 @@
-import React, { memo, useEffect, useRef } from 'react';
-import { useDispatch, useSelector, shallowEqual } from 'react-redux';
+import React, {memo, useEffect, useRef} from 'react';
+import {useDispatch, useSelector, shallowEqual} from 'react-redux';
 
-import { getNewAlbumAction } from '../../store/actionCreators';
+import {getNewAlbumAction} from '../../store/action';
 
-import { Carousel } from 'antd';
+import {Carousel} from 'antd';
 import HYAlbumCover from '@/components/album-cover';
-import HYThemeHeaderRCM from '@/components/theme-header-rcm';
-import { AlbumWrapper } from './style';
+import ThemeHeaderRCM from '@/components/theme-header-rcm';
+import {AlbumWrapper} from './style';
 
-export default memo(function HYNewAlbum() {
-  // redux hooks
-  const { newAlbums } = useSelector(state => ({
-    newAlbums: state.getIn(["recommend", "newAlbums"])
-  }), shallowEqual);
-  const dispatch = useDispatch();
+import {getNewAlbumsAction} from '../../store/action';
 
-  // other hooks
-  const pageRef = useRef();
-  useEffect(() => {
-    dispatch(getNewAlbumAction(10));
-  }, [dispatch]);
+export default memo(function NewAlbum() {
+    //state
 
-  return (
-    <AlbumWrapper>
-      <HYThemeHeaderRCM title="新碟上架" />
-      <div className="content">
-        <button className="arrow arrow-left sprite_02" 
-                onClick={e => pageRef.current.prev()}></button>
-        <div className="album">
-          <Carousel dots={false} ref={pageRef}>
-            {
-              [0, 1].map(item => {
-                return (
-                  <div key={item} className="page">
-                    {
-                      newAlbums.slice(item * 5, (item + 1) * 5).map(iten => {
-                        return <HYAlbumCover key={iten.id} 
-                                             info={iten} 
-                                             size={100} 
-                                             width={118} 
-                                             bgp="-570px"/>
-                      })
-                    }
-                  </div>
-                )
-              })
-            }
-          </Carousel>
-        </div>
-        <button className="arrow arrow-right sprite_02"
-                onClick={e => pageRef.current.next()}></button>
-      </div>
-    </AlbumWrapper>
-  )
+    //redux
+    const dispatch = useDispatch();
+    const {albums} = useSelector((state) => ({
+        albums: state.getIn(['recommend', 'newAlbums']),
+    }), shallowEqual)
+
+    //others
+    useEffect(() => {
+        dispatch(getNewAlbumsAction(10))
+    }, [dispatch]);
+    const pageRef = useRef();
+
+    return (
+        <AlbumWrapper>
+            <ThemeHeaderRCM title={'新品'}/>
+            <div className="content">
+                <button className="arrow arrow-left sprite_02"
+                        onClick={e => pageRef.current.prev()}
+                />
+                <div className="album">
+                    <Carousel dots={false} ref={pageRef}>
+                        {
+                            [0, 1].map(item => {
+                                return <div key={item} className="page">
+                                    {
+                                        albums.slice(item * 5, (item + 1) * 5).map(i => {
+                                            return <HYAlbumCover key={i.id}
+                                                                 info={i}
+                                                                 size={100}
+                                                                 width={118}
+                                                                 bgp="-570px" />
+                                        })
+                                    }
+                                </div>
+                            })
+                        }
+                    </Carousel>
+                </div>
+
+                <button className="arrow arrow-right sprite_02"
+                        onClick={e => pageRef.current.next()}
+                />
+            </div>
+        </AlbumWrapper>
+    )
 })
